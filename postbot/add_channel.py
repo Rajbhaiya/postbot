93d47b040c062fdafab97239e68017b645e2a3ec
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-21 00:37:30 +0530

    Update add_channel.py

--- postbot/add_channel.py
+++ postbot/add_channel.py
@@ -12,11 +12,9 @@ async def add_channel_callback(bot, callback_query):
     channel_id = int(callback_query.data.split('_')[2])
 
     try:
-        # Check if the bot is an admin in the channel
         bot_chat_member = await bot.get_chat_member(channel_id, bot.me.id)
 
         if bot_chat_member.status == "administrator":
-            # Ask the user to forward a message from the desired channel
             forward_message = await bot.ask(user_id,
                 "Please forward a message from the channel you want to add. "
                 "After forwarding, I will check and complete the process. "
@@ -25,29 +23,35 @@ async def add_channel_callback(bot, callback_query):
             )
 
             if forward_message.forward_from_chat.type == "channel":
-                # The forwarded message is from a channel
                 channel_to_add_id = forward_message.forward_from_chat.id
 
                 try:
                     channel_member = await bot.get_chat_member(channel_to_add_id, user_id)
                     if channel_member.status == "administrator":
-                        # The user is an admin in the channel they want to add
-                        # Implement your database and channel management logic here
-                        # You can add the user to the channel and perform other actions
 
-                        # Respond to the user with a success message
-                        await callback_query.answer("Channel added successfully!")
-
-                        # You can also send additional messages or perform other actions here
+                        # Database Logic - Check if the channel is already in the user's list
+                        user = Users.get(user_id)
+                        if user:
+                            if channel_id in user.channels:
+                                await forward_message.reply("Channel is already added in bot.")
+                            else:
+                                user.add_channel(channel_id)
+                                await forward_message.reply("Channel added successfully!")
+                        else:
+                            user = Users(user_id)
+                            user.add_channel(channel_id)
+                            await forward_message.reply("Channel added successfully!")
                     else:
-                        await callback_query.answer("You are not an admin in the channel you want to add.")
+                        await forward_message.reply("You are not an admin in the channel you want to add.")
                 except UserNotParticipant:
-                    await callback_query.answer("You are not a member of the channel you want to add.")
+                    await forward_message.reply("You are not a member of the channel you want to add.")
                 except ChatAdminRequired:
-                    await callback_query.answer("Bot is not an admin in the channel.")
+                    await forward_message.reply("Bot is not an admin in the channel.")
             else:
-                await callback_query.answer("Please forward a message from a channel.")
+                await forward_message.reply("Please forward a message from a channel.")
         else:
-            await callback_query.answer("Bot is not an admin in the channel.")
+            await forward_message.reply("Bot is not an admin in the channel.")
     except ChatAdminRequired:
-        await callback_query.answer("Bot is not an admin in the channel.")
+        await forward_message.reply("Bot is not an admin in the channel.")
+    except BadRequest as e:
+        await bot.send_message(user_id, f"Error: {e}")
