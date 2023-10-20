5dd1a1af01d4b1113afb61a73aa6a928c77291bc
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-20 20:12:29 +0530

    Create __init__.py

---
+++ postbot/database/__init__.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from pymongo.database import Database
+from Config import MONGODB_URI, MONGODB_DATABASE_NAME
+
+def start_mongodb() -> Database:
+    client = MongoClient(MONGODB_URI)
+    db = client[MONGODB_DATABASE_NAME]
+    return db
+
+MONGODB_DB = start_mongodb()
