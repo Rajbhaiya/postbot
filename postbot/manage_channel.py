0c151a8ab2dcfe6d59e2668610762cb7ffcd81c7
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-21 01:37:26 +0530

    Update manage_channel.py

--- postbot/manage_channel.py
+++ postbot/manage_channel.py
@@ -6,52 +6,54 @@ from __main__ import bot
 from postbot.datababe.db_channel import *
 from postbot.datababe.db_users import *
 
-# Define the add_channel function (as shown in the previous response)
-
-# Add a new callback function to manage and delete channels
 @bot.on_callback_query(filters.regex(r'^manage_channels$'))
 async def manage_channels_callback(bot, callback_query):
     user_id = callback_query.from_user.id
-    status, channels = await get_channels(user_id)
 
-    if status:
-        text = "Below are your channels. You can manage them by deleting a channel from the list."
-        buttons = []
+    # Get the user's channels
+    user = Users.get(user_id)
+    if not user or not user.channels:
+        await callback_query.answer("No channels found in your list.")
+        return
 
-        for channel in channels:
-            try:
-                chat = await bot.get_chat(channel)
-                buttons.append([InlineKeyboardButton(f"🗑 Delete {chat.title}", callback_data=f'delete_channel_{channel}')])
-            except ChannelInvalid:
-                continue
+    buttons = []
+    for channel_id in user.channels:
+        try:
+            chat = await bot.get_chat(channel_id)
+            # Add a button for each channel
+            buttons.append([InlineKeyboardButton(chat.title, callback_data=f'channel_options_{channel_id}')])
+        except ChannelInvalid:
+            continue
 
-        buttons.append([InlineKeyboardButton("Back", callback_data="start")])
+    buttons.append([InlineKeyboardButton("Back", callback_data="start")])
 
-        await callback_query.edit_message_text(text, reply_markup=InlineKeyboardMarkup(buttons))
-    else:
-        await callback_query.answer("No Channels Found. Add a channel using the provided button.")
+    await callback_query.edit_message_text("Your channels:", reply_markup=InlineKeyboardMarkup(buttons))
 
-# Add a new callback function to delete a channel
+@bot.on_callback_query(filters.regex(r'^channel_options_\d+$'))
+async def channel_options_callback(bot, callback_query):
+    user_id = callback_query.from_user.id
+    channel_id = int(callback_query.data.split('_')[2])
+
+    buttons = [
+        [InlineKeyboardButton("Delete Channel", callback_data=f'delete_channel_{channel_id}')],
+        [InlineKeyboardButton("Back", callback_data="manage_channels")]
+    ]
+
+    await callback_query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
+
 @bot.on_callback_query(filters.regex(r'^delete_channel_\d+$'))
 async def delete_channel_callback(bot, callback_query):
     user_id = callback_query.from_user.id
     channel_id = int(callback_query.data.split('_')[2])
-    
-    # Database Logic - Remove the channel from the user's list
+
+    # Remove the channel from the user's database
     user = Users.get(user_id)
-    
-    if user and channel_id in user.channels:
-        user.remove_channel(channel_id)
-        await callback_query.answer("Channel deleted successfully.")
-    else:
-        await callback_query.answer("Channel not found in the database.")
+    if user and user.channels and channel_id in user.channels:
+        user.channels.remove(channel_id)
+        user.save()
 
-@bot.on_callback_query(filters.regex(r'^add_channel_\d+$'))
-async def add_channel_callback(bot, callback_query):
-    user_id = callback_query.from_user.id
-    channel_id = int(callback_query.data.split('_')[2])
+    # Notify the user
+    await callback_query.answer("Channel deleted successfully.")
 
-    # Call the add_channel function
-    await add_channel(user_id, channel_id)
-    await callback_query.answer("Processing your request...")  
-
+    # Go back to the "Manage Channels" menu
+    await manage_channels_callback(bot, callback_query)
