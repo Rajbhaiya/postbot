499e2a2040b5d70dffa194d5b4189923d0ec02e7
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-21 15:23:49 +0530

    Create db_reaction.py

---
+++ postbot/database/db_reaction.py
@@ -0,0 +1,37 @@
+# db_reaction.py
+
+from postbot.database import db
+MONGODB_DB = db['reactions']
+
+class Reaction:
+    def __init__(self, channel_id, post_id, emoji, count):
+        self.channel_id = channel_id
+        self.post_id = post_id
+        self.emoji = emoji
+        self.count = count
+
+    def save(self):
+        MONGODB_DB.reactions.insert_one({
+            'channel_id': self.channel_id,
+            'post_id': self.post_id,
+            'emoji': self.emoji,
+            'count': self.count
+        })
+
+    # Add other methods to update and retrieve reactions as needed
+
+    @classmethod
+    def get(cls, channel_id, post_id, emoji):
+        reaction_data = MONGODB_DB.reactions.find_one({
+            'channel_id': channel_id,
+            'post_id': post_id,
+            'emoji': emoji
+        })
+        if reaction_data:
+            return cls(
+                reaction_data['channel_id'],
+                reaction_data['post_id'],
+                reaction_data['emoji'],
+                reaction_data['count']
+            )
+        return None
