918551044f9b5e4c0b8db65918f17842c966b515
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-21 19:31:39 +0530

    Update __init__.py

--- postbot/database/__init__.py
+++ postbot/database/__init__.py
@@ -3,11 +3,11 @@ import pymongo
 import logging
 from pymongo.errors import ServerSelectionTimeoutError
 from motor import motor_asyncio
-from config import *
+import Config
 
 mongodb_client = pymongo.MongoClient(DB_URL, 27017)
 motor = motor_asyncio.AsyncIOMotorClient(DB_URL, 27017)
-db = mongodb_client["kagut"]
+db = mongodb_client["postbot"]
 
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
