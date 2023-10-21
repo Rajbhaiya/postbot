8b7ff92f29a39291a3e7a46292dd68f07b930e4f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-21 18:00:16 +0530

    Update start.py

--- postbot/start.py
+++ postbot/start.py
@@ -1,1 +1,24 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from .add_channel import add_channel_callback
+from .manage_channel import manage_channels_callback
+from .send_post import send_post_callback
 
+@bot.on_message(filters.command("start"))
+async def start(bot, message):
+    # Define the keyboard with three buttons
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Add Channel", callback_data="add_channel"),
+                InlineKeyboardButton("Manage Channel", callback_data="manage_channels"),
+            ],
+            [InlineKeyboardButton("Send Post", callback_data="send_post")],
+        ]
+    )
+
+    # Send a welcome message with the keyboard
+    await message.reply("Welcome to your bot!\nPlease choose an action:", reply_markup=keyboard)
+
+if __name__ == "__main__":
+    bot.run()
