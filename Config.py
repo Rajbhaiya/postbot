910b2ff440774153262b10725437f5fdad33176e
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-21 18:06:00 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+import loguru
+LOGGER = loguru.logger
+
+API_ID = 13675555 
+API_HASH = "c0da9c346d2c45dbc7ec49a05da9b2b6" 
+TOKEN = "6680969743:AAHpx2FWxrJDDBZTasyyUk05h7a0zG6aeMc" 
+DB_URL = "mongodb+srv://kagut:kagut@cluster0.hol7gj5.mongodb.net/?retryWrites=true&w=majority"
