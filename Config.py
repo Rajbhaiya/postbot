649d8bbc76166d610d91047c5f11c06414afb8de
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-21 19:42:58 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,5 +3,5 @@ LOGGER = loguru.logger
 
 API_ID = 13675555 
 API_HASH = "c0da9c346d2c45dbc7ec49a05da9b2b6" 
-TOKEN = "6680969743:AAHpx2FWxrJDDBZTasyyUk05h7a0zG6aeMc" 
+BOT_TOKEN = "6680969743:AAHpx2FWxrJDDBZTasyyUk05h7a0zG6aeMc" 
 DB_URL = "mongodb+srv://kagut:kagut@cluster0.hol7gj5.mongodb.net/?retryWrites=true&w=majority"
