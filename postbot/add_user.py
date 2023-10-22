ef7ab23eee93a6aa62bfbf4f7d4de9ecc5c489a5
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-22 15:03:54 +0530

    Update add_user.py

--- postbot/add_user.py
+++ postbot/add_user.py
@@ -1,8 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from .database.db_users import Users, num_users
-from .database import SESSION
+from .database.db_users import Users, users_count, USERS_MONGODB_DB
 from Config import OWNER_ID
 
 @Client.on_message(~filters.service, group=1)
@@ -10,14 +9,12 @@ async def users_mongodb(_, msg: Message):
     if msg.from_user:
         user_id = msg.from_user.id
         # Check if the user exists in the database
-        user_data = users_collection.find_one({"_id": user_id})
+        user_data = USERS_MONGODB_DB.find_one({"_id": user_id})
         if not user_data:
             # If the user doesn't exist, insert a new document
-            users_collection.insert_one({"_id": user_id})
+            USERS_MONGODB_DB.insert_one({"_id": user_id})
         # No need to close the session
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
-    # Get the total number of users from the database
-    users_count = users_collection.count_documents({})
     await msg.reply(f"Total Users: {users_count}", quote=True)
