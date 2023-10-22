8167912aa7640595d14f613e3c499d7af3e6a2cc
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-23 01:09:42 +0530

    Update start.py

--- postbot/start.py
+++ postbot/start.py
@@ -11,7 +11,7 @@ async def start(bot, message:Message):
     keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("Add Channel", callback_data="add_channel_1"),
+                InlineKeyboardButton("Add Channel", callback_data="add_channel"),
                 InlineKeyboardButton("Manage Channel", callback_data="manage_channels"),
             ],
             [InlineKeyboardButton("Send Post", callback_data="send_post_1")],
