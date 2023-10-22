5e30d8969fc33c531c8f654fcbbf432c9dc45353
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-23 02:12:48 +0530

    Update start.py

--- postbot/start.py
+++ postbot/start.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from .add_channel import *
+from .add_channels import *
 from .manage_channel import *
 from .send_post import *
 from postbot import bot
