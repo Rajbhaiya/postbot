7ff131ed991bc281157389abe1fb035b93d75a3a
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-22 15:10:23 +0530

    Update add_user.py

--- postbot/add_user.py
+++ postbot/add_user.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from .database.db_users import Users, users_count, USERS_MONGODB_DB
+from .database.db_users import Users, USERS_MONGODB_DB
 from Config import OWNER_ID
 
 @Client.on_message(~filters.service, group=1)
@@ -17,4 +17,5 @@ async def users_mongodb(_, msg: Message):
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
+     users_count = USERS_MONGODB_DB.count_documents({})
     await msg.reply(f"Total Users: {users_count}", quote=True)
