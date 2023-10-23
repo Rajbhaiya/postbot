c2be90c4e927f5f69813b058008ecf4c684272e6
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-24 00:07:57 +0530

    Update __main__.py

--- postbot/__main__.py
+++ postbot/__main__.py
@@ -1,11 +1,21 @@
 from pyromod import listen
+import pyrogram
 from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 from Config import LOGGER
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the desired log level
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+# Create a logger for Pyrogram
+pyrogram_logger = logging.getLogger('pyrogram')
+pyrogram_logger.setLevel(logging.INFO)  # Set the log level for Pyrogram
 
 from postbot import bot, add_channels, manage_channel, send_post, start, setting, add_user
-
-
 
 # Run Bot
 if __name__ == "__main__":
