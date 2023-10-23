6f0980f9341a07484946bc4580c8352fb0949601
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-23 15:37:29 +0530

    Update db_users.py

--- postbot/database/db_users.py
+++ postbot/database/db_users.py
@@ -1,67 +1,58 @@
 from postbot.database import db
 
-USERS_MONGODB_DB = db['users']
+USERS_DB = db['users']
 
-class Users:
-    def __init__(self, user_id, channels=None):
-        self.user_id = user_id
-        self.channels = channels if channels is not None else []
+def init_user(user_id, channels=None):
+    return {
+        'user_id': user_id,
+        'channels': channels if channels is not None else []
+    }
 
-    async def save(self):
-        await USERS_MONGODB_DB.users.insert_one({
-            'user_id': self.user_id,
-            'channels': self.channels
-        })
+async def save_user(user_id, channels=None):
+    user_data = init_user(user_id, channels)
+    await USERS_DB.users.insert_one(user_data)
 
-    async def update(self):
-        await USERS_MONGODB_DB.users.update_one(
-            {'user_id': self.user_id},
-            {'$set': {
-                'channels': self.channels
-            }}
-        )
+async def update_user(user_id, channels):
+    await USERS_DB.users.update_one(
+        {'user_id': user_id},
+        {'$set': {
+            'channels': channels
+        }}
+    )
 
-    @classmethod
-    async def get(cls, user_id):
-        user_data = await USERS_MONGODB_DB.users.find_one({'user_id': user_id})
-        if user_data:
-            return cls(
-                user_data['user_id'],
-                user_data['channels']
-            )
-        return None
+async def get_user(user_id):
+    user_data = await USERS_DB.users.find_one({'user_id': user_id})
+    if user_data:
+        return user_data
+    return None
 
-    @classmethod
-    async def add_channel(cls, user_id, channel_id):
-        user = await cls.get(user_id)
-        if user:
-            if user.channels:
-                channels = list(set(user.channels))
-                channels.append(channel_id)
-                user.channels = channels
-            else:
-                user.channels = [channel_id]
-            await user.update()
+async def add_channel(user_id, channel_id):
+    user = await get_user(user_id)
+    if user:
+        if user['channels']:
+            channels = list(set(user['channels']))
+            channels.append(channel_id)
+            user['channels'] = channels
         else:
-            user = cls(user_id, [channel_id])
-            await user.save()
+            user['channels'] = [channel_id]
+        await update_user(user_id, user['channels'])
+    else:
+        user = init_user(user_id, [channel_id])
+        await save_user(user_id, user['channels'])
 
-    @classmethod
-    async def remove_channel(cls, user_id, channel_id):
-        user = await cls.get(user_id)
-        if user and user.channels:
-            if channel_id in user.channels:
-                user.channels.remove(channel_id)
-                if not user.channels:
-                    user.channels = None
-                await user.update()
+async def remove_channel(user_id, channel_id):
+    user = await get_user(user_id)
+    if user and user['channels']:
+        if channel_id in user['channels']:
+            user['channels'].remove(channel_id)
+            if not user['channels']:
+                user['channels'] = None
+            await update_user(user_id, user['channels'])
 
-    @classmethod
-    async def get_channels(cls, user_id):
-        user = await cls.get(user_id)
-        return user.channels if user else []
+async def get_channels(user_id):
+    user = await get_user(user_id)
+    return user['channels'] if user else []
 
-    @classmethod
-    async def users_count(cls):
-        # Calculate and return the number of users
-        return await USERS_MONGODB_DB.users.count_documents({})
+async def users_count():
+    # Calculate and return the number of users
+    return await USERS_DB.users.count_documents({})
