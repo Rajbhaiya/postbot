2c3de8850b3ef7f77687a6604bc835afe58b4733
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-23 19:41:46 +0530

    Update setting.py

--- postbot/setting.py
+++ postbot/setting.py
@@ -7,8 +7,8 @@ from postbot import bot
 @bot.on_callback_query(filters.regex(r'^channel_settings_\d+$'))
 async def channel_settings_callback(bot, callback_query: CallbackQuery):
     channel_id = int(callback_query.data.split('_')[2])
-
-    text, markup, sticker_id = await channel_settings(channel_id, bot)
+    text = f'**{title}** (`{channel_id}
+    text, markup, sticker_id = await get_channel_info(channel_id)
 
     if text:
         await callback_query.message.edit_text(text, reply_markup=InlineKeyboardMarkup(markup), quote=True)
