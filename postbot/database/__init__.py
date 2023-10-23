1101b2961b49d251ef598702256caae4c2cd3509
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-24 00:06:05 +0530

    Update __init__.py

--- postbot/database/__init__.py
+++ postbot/database/__init__.py
@@ -1,11 +1,25 @@
 import asyncio
+import time
 import logging
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.errors import ServerSelectionTimeoutError
 from Config import DB_URL
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+# Create a logger for your application
+logger = logging.getLogger('postbot')
+
 # Create an AsyncIOMotorClient instance
 motor = AsyncIOMotorClient(DB_URL, 27017)
 
-# Access the database using the motor client
-db = motor["postbot"]
+try:
+    # Access the database using the motor client
+    db = motor["postbot"]
+    logger.info("Connected to the MongoDB database.")
+except ServerSelectionTimeoutError:
+    logger.error("Failed to connect to the MongoDB database.")
