d59ecca268c33947ddefe8e51d4647cd65abf26e
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-24 00:01:00 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,4 +5,4 @@ API_ID = 13675555
 OWNER_ID = 5591954930
 API_HASH = "c0da9c346d2c45dbc7ec49a05da9b2b6" 
 BOT_TOKEN = "6680969743:AAHpx2FWxrJDDBZTasyyUk05h7a0zG6aeMc" 
-DB_URL = "mongodb+srv://kagut:kagut@cluster0.hol7gj5.mongodb.net/?retryWrites=true&w=majority"
+DB_URL = "mongodb+srv://f2l:f2l@cluster0.fjjge1y.mongodb.net/?retryWrites=true&w=majority"
