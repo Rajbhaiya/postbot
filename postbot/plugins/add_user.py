d17e91228c0c8e9cb71f5d1184f54100e1bc0a93
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-24 16:42:46 +0530

    Add files via upload

---
+++ postbot/plugins/add_user.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from .database.db_users import save_user, get_user, users_count
+from Config import OWNER_ID
+from postbot import bot
+
+@bot.on_message(~filters.service, group=1)
+async def users_mongodb(_, msg: Message):
+    if msg.from_user:
+        user_id = msg.from_user.id
+        find_user = await get_user(user_id)
+        if not find_user:
+            await save_user(user_id)
+
+@bot.on_message(filters.user(OWNER_ID) & filters.command("stats"))
+async def _stats(_, msg: Message):
+    x = await users_count()
+    await msg.reply(f"Total Users: {x}", quote=True)
---
+++ postbot/plugins/setting.py
@@ -0,0 +1,59 @@
+from pyrogram import emoji, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from postbot.database.db_channel import *
+from postbot import bot
+
+
+@bot.on_callback_query(filters.regex(r'^channel_settings_\d+$'))
+async def channel_settings_callback(bot, callback_query: CallbackQuery):
+    channel_id = int(callback_query.data.split('_')[2])
+
+    # Fetch the channel information from the Telegram API
+    chat = await bot.get_chat(channel_id)
+    channel_title = chat.title
+
+    text, markup, sticker_id = await get_channel_info(channel_id)
+
+    if text:
+        await callback_query.message.edit_text(f"**{channel_title}** (`{channel_id}`)\n{text}", reply_markup=InlineKeyboardMarkup(markup), quote=True)
+    else:
+        await callback_query.message.delete()
+
+@bot.on_callback_query(filters.regex(r'^edit_emojis_\d+$'))
+async def edit_emojis_callback(bot, callback_query: CallbackQuery):
+    user_id = callback_query.from_user.id
+    channel_id = int(callback_query.data.split('_')[2])
+
+    # Fetch the current emojis from the database
+    success, info = await get_channel_info(channel_id)
+    emojis = info.get('emojis', [])
+
+    text = f"Current Emojis: {', '.join(emojis)}\n\n"
+    text += "Send the new emojis separated by commas (e.g., 😀, 😂, 😍)."
+
+    # Ask the user to send the new emojis
+    new_emojis_message = await bot.ask(user_id, text, timeout=300)
+
+    if new_emojis_message.text:
+        new_emojis = [emoji.strip() for emoji in new_emojis_message.text.split(",")]
+        await add_emojis(channel_id, new_emojis)
+        await callback_query.answer("Emojis updated successfully!")
+    else:
+        await callback_query.answer("Invalid input. Please send emojis separated by commas.")
+
+@bot.on_callback_query(filters.regex(r'^edit_sticker_\d+$'))
+async def edit_sticker_callback(bot, callback_query: CallbackQuery):
+    user_id = callback_query.from_user.id
+    channel_id = int(callback_query.data.split('_')[2])
+
+    text = "Please send the sticker you want to set for this channel."
+
+    # Ask the user to send the sticker
+    sticker_message = await bot.ask(user_id, text, timeout=300)
+
+    if sticker_message.sticker:
+        sticker_id = sticker_message.sticker.file_id
+        await set_sticker(channel_id, sticker_id)
+        await callback_query.answer("Sticker set successfully!")
+    else:
+        await callback_query.answer("Invalid input. Please send a sticker.")
