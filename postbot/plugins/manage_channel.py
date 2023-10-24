5b3fe7d2e4cc606b04be7287c5df3231dcc689f8
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-24 17:02:39 +0530

    Update manage_channel.py

--- postbot/plugins/manage_channel.py
+++ postbot/plugins/manage_channel.py
@@ -7,41 +7,53 @@ from postbot.database.db_users import *
 
 # ... (other code and imports) ...
 
-@bot.on_callback_query(filters.regex(r'^manage_channels$'))
+bot.on_callback_query(filters.regex(r'^manage_channels$'))
 async def manage_channels_callback(bot, callback_query: CallbackQuery):
     user_id = callback_query.from_user.id
 
-    # Get the user's channels
-    user = await get_user(user_id)
-    if not user or not user['channels']:  # Fix: Access 'channels' as a dictionary key
-        await callback_query.answer("No channels found in your list.")
-        return
+    try:
+        # Get the user's channels
+        user = await get_user(user_id)
+        if not user or not user.get('channels'):
+            await callback_query.answer("No channels found in your list.")
+            return
 
-    buttons = []
-    for channel_id in user['channels']:  # Fix: Access 'channels' as a dictionary key
-        try:
-            chat = await bot.get_chat(channel_id)
-            # Add a button for each channel
-            buttons.append([InlineKeyboardButton(chat.title, callback_data=f'channel_options_{channel_id}')])
-        except ChannelInvalid:
-            continue
+        buttons = []
+        for channel_id in user['channels']:
+            try:
+                chat = await bot.get_chat(channel_id)
+                # Add a button for each channel
+                buttons.append([InlineKeyboardButton(chat.title, callback_data=f'channel_options_{channel_id}')])
+            except ChannelInvalid:
+                continue
 
-    buttons.append([InlineKeyboardButton("Back", callback_data="start_data")])
+        buttons.append([InlineKeyboardButton("Back", callback_data="manage_channels")])
 
-    await callback_query.edit_message_text("Your channels:", reply_markup=InlineKeyboardMarkup(buttons))
+        await callback_query.edit_message_text("Your channels:", reply_markup=InlineKeyboardMarkup(buttons))
+
+    except Exception as e:
+        # Handle any exceptions and log them
+        print(f"Error in manage_channels_callback: {e}")
+        await callback_query.answer("An error occurred. Please try again later.")
 
 @bot.on_callback_query(filters.regex(r'^channel_options_\d+$'))
 async def channel_options_callback(bot, callback_query: CallbackQuery):
     user_id = callback_query.from_user.id
     channel_id = int(callback_query.data.split('_')[2])
 
-    buttons = [
-        [InlineKeyboardButton("Channel Settings", callback_data=f'channel_settings_{channel_id}')],
-        [InlineKeyboardButton("Delete Channel", callback_data=f'delete_channel_{channel_id}')],
-        [InlineKeyboardButton("Back", callback_data="manage_channels")]
-    ]
+    try:
+        buttons = [
+            [InlineKeyboardButton("Channel Settings", callback_data=f'channel_settings_{channel_id}')],
+            [InlineKeyboardButton("Delete Channel", callback_data=f'delete_channel_{channel_id}')],
+            [InlineKeyboardButton("Back", callback_data="manage_channels")]
+        ]
 
-    await callback_query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
+        await callback_query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
+
+    except Exception as e:
+        # Handle any exceptions and log them
+        print(f"Error in channel_options_callback: {e}")
+        await callback_query.answer("An error occurred. Please try again later.")
 
 @bot.on_callback_query(filters.regex(r'^delete_channel_\d+$'))
 async def delete_channel_callback(bot, callback_query: CallbackQuery):
