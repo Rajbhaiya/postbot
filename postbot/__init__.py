d68a806d15385ebc16e319132a89718eca178c69
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-10-24 16:43:46 +0530

    Update __init__.py

--- postbot/__init__.py
+++ postbot/__init__.py
@@ -1,4 +1,4 @@
 from Config import *
 from pyromod import listen
 from pyrogram import Client 
-bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="postbot.plugins"))
